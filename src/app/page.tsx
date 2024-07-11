25eb4cd8e627fab8ceeff288a4bf58d7db3bf709
Your Name <your.email@example.com>
2024-07-11 13:59:57 +0530

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { PinContainer } from "@/components/customUi/3d-pin";
 import { TextGenerateEffect } from "@/components/customUi/text-generate-effect";
+import { SignupForm } from "@/components/pages/SIgnUpForm.tsx";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -8,30 +9,32 @@ export default function Home() {
 `;
   return (
     <>
-    <div className="mx-auto max-w-lg w-full py-20">
-    <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
-      >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
-          </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
-            </span>
+      <div className="mx-auto max-w-lg w-full py-20">
+        <PinContainer
+          title="/ui.aceternity.com"
+          href="https://twitter.com/mannupaaji"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              Aceternity UI
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">
+                Customizable Tailwind CSS and Framer Motion Components.
+              </span>
+            </div>
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-        </div>
-      </PinContainer>
-      <div className="my-10"></div>
-      <TextGenerateEffect words={words} />
-      <div className="my-10"></div>
-      <Button>
-        Loading
-      </Button>
-    </div>
+        </PinContainer>
+        <div className="my-10"></div>
+        <TextGenerateEffect words={words} />
+        <div className="my-10"></div>
+        <Button>
+          Loading
+          <div className="my-10"></div>
+       
+        </Button>
+      </div>
     </>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from "@/components/pages/SIgnUpForm.tsx";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default page;
